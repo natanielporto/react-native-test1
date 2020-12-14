48b76ae85793dcfc498043cb6b80ec126a657a52
Nataniel Porto <natanielkporto@gmail.com>
2020-12-14 17:41:18 -0300

    Add right text for the button

--- src/index.js
+++ src/index.js
@@ -1,33 +1,51 @@
-import React, {useState, useCallback} from 'react'
-import {View, Text, StyleSheet, StatusBar, TouchableOpacity} from 'react-native'
+import React, {useState, useCallback} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
 
-const COLORS = ['white', 'black', 'blue', 'green', 'pink', 'red', 'purple', 'yellow', 'grey', 'lilac'];
+const COLORS = [
+  'white',
+  'black',
+  'blue',
+  'green',
+  'pink',
+  'red',
+  'purple',
+  'yellow',
+  'grey',
+  'lilac',
+];
 
 export default function App() {
-  const [color, setColor] = useState('white')
+  const [color, setColor] = useState('white');
 
   const randomColor = useCallback(() => {
     const randomNumber = new Date().getTime();
     const lastNumber = randomNumber.toString().slice(-1);
-    
+
     setColor(COLORS[+lastNumber]);
   }, [setColor]);
 
   return (
     <>
-      <StatusBar barStyle="light-content"/>
-        <View style={{backgroundColor: color, flex: 1}} />
+      <StatusBar barStyle="light-content" />
+      <View style={{backgroundColor: color, flex: 1}} />
 
-      <TouchableOpacity style={styles.button} activeOpacity={0.5} onPress={randomColor}>
-        <Text style={styles.buttonText}>
-        Toque para randomizar cor do fundo
-        </Text>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.5}
+        onPress={randomColor}>
+        <Text style={styles.buttonText}>Go 4all</Text>
       </TouchableOpacity>
     </>
-  )
+  );
 }
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
@@ -36,10 +54,10 @@ const styles = StyleSheet.create ({
     height: 50,
     borderRadius: 4,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   buttonText: {
     fontWeight: 'bold',
-    fontSize: 16
-  }
-})
+    fontSize: 16,
+  },
+});
